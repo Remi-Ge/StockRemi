12d6b20acbed5d263dbca48611801aeeea8952eb
Remi-Ge <140516143+Remi-Ge@users.noreply.github.com>
2025-02-12 17:19:31 +0100

    fix: fix PIECE_MOVEMENTS initialization

--- src/move.cpp
+++ src/move.cpp
@@ -7,18 +7,17 @@ Move::Move(u_int8_t startSquare, u_int8_t targetSquare) {
 
 const std::map<PieceType, std::vector<PieceMovements>> Move::PIECE_MOVEMENTS = {
     {PieceType::rook, {
-        {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, true, false, 7}
+        PieceMovements{{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}, true, false, 7}
     }}, {PieceType::knight, {
-        {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, true, false, 1}
+        PieceMovements{{{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}}, true, false, 1}
     }}, {PieceType::bishop, {
-        {{1, 1}, {-1, -1}, {1, -1}, {-1, 1}, true, false, 7}
+        PieceMovements{{{1, 1}, {-1, -1}, {1, -1}, {-1, 1}}, true, false, 7}
     }}, {PieceType::queen, {
-        {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}, true, false, 7}
+        PieceMovements{{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}, true, false, 7}
     }}, {PieceType::king, {
-        {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}, true, false, 1}
+        PieceMovements{{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {1, -1}, {-1, 1}}, true, false, 1}
     }}, {PieceType::pawn, {
-        {{0, -1}, false, false, 1},  // Forward (for white)
-        {{-1, -1}, {1, -1}, true, true, 1}  // Takes
+        PieceMovements{{{0, -1}}, false, false, 1},  // Forward (for white !!)
+        PieceMovements{{{-1, -1}, {1, -1}}, true, true, 1}  // Takes
     }},
 };
-
